34a49bbec136e33a5e3af1b436f1ab231a353f95
Sekhar <192883287+chandra-samal@users.noreply.github.com>
2025-05-21 07:23:09 +0530

    Raspberries | 1000 | Better Solution

--- raspberries.cpp
+++ raspberries.cpp
@@ -13,48 +13,30 @@ int main() {
         cin >> n >> k;
         int noOfOperations = 0;
         int arr[n];
-        bool isAlreadyDivisible=false;
         int evenCount = 0;
-        int closestToFive = 0;
-        int closestToTen = INT_MIN;
-        bool threeOrSevenPresent = false;
-        vector<int> remainderWithThree;
+        vector<int> remainders;
+        bool isAlreadyDivisible = false;
         for (int i = 0; i<n; i++){
             cin >> arr[i];
-            if (arr[i]%2==0){
-                evenCount++;
-            }
-            if (arr[i]==3 || arr[i]==7) threeOrSevenPresent = true;
-            remainderWithThree.emplace_back(arr[i]%3);
-            if (arr[i]>closestToTen) closestToTen=arr[i];
-            if ((arr[i]<=5) && (arr[i]>closestToFive)) closestToFive=arr[i];
-            if (arr[i]%k==0) {
-                // cout << noOfOperations << '\n';
-                isAlreadyDivisible = true;
-                // break;
-            }
+            if (arr[i]%2==0) evenCount++;
+            if (arr[i]%k==0) isAlreadyDivisible = true;
+            remainders.push_back(arr[i]%k);
         }
-        if (isAlreadyDivisible) {
+        if (isAlreadyDivisible){
             cout << noOfOperations << '\n';
             continue;
         }
-        // cout << "Closest to Five: " << closestToFive << " Closest to Ten: " << closestToTen << "\n";
-        sort(remainderWithThree.begin(), remainderWithThree.end());
-        // k = 2, 3, 4, 5
-        if (k==2){
-            if (evenCount<1) noOfOperations = 1;
+
+        sort(remainders.begin(), remainders.end());
+
+        if (k==4){
+            if (evenCount>2) evenCount=2;
+            int n1 = 2-evenCount;
+            int n2 = 4-remainders[remainders.size()-1];
+            noOfOperations = (n1<n2)?n1:n2;
         }
-        else if (k==4){
-            if (threeOrSevenPresent && evenCount==0) noOfOperations = 1;
-            else if (evenCount<2) noOfOperations = 2-evenCount;
-        }
-        else if (k==5){
-            noOfOperations = (10-closestToTen<5-closestToFive)?10-closestToTen:5-closestToFive;
-            if (closestToFive==0) noOfOperations = 10-closestToTen;
-        }
-        else if (k==3){
-            noOfOperations = 3 - remainderWithThree[remainderWithThree.size()-1];
-        }
+        else noOfOperations = k-remainders[remainders.size()-1];
+
         cout << noOfOperations << '\n';
     }
 
